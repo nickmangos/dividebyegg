3622d6dda30496ea0d86f6ffd42102748ae780bf
Nick Mangos <nick.mangos@gmail.com>
2022-05-16 12:37:30 +1200

    Configure Webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,24 +1,57 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const Dotenv = require('dotenv-webpack')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 module.exports = {
-  entry: path.join(__dirname, 'index.js'),
+  entry: ['./client/index.js', './client/styles/index.scss'],
   output: {
-    path: path.join(__dirname, '../server/public'),
+    path: path.join(__dirname, '..', 'server', 'public'),
     filename: 'bundle.js'
   },
   mode: 'development',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false // Enable to remove warnings about conflicting order
+    }),
+    new Dotenv({
+      path: path.join(__dirname, '..', 'server', '.env')
+    }),
+    new ReactRefreshWebpackPlugin(),
+  ],
   module: {
-    rules: [{
-      test: /\.jsx?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    }]
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          plugins: ['react-refresh/babel'],
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
   },
   resolve: {
     extensions: ['.js', '.jsx']
   },
-  devtool: 'source-map',
-  devServer: {
-    contentBase: path.join(__dirname, '../server/public')
-  }
+  devtool: 'source-map'
 }
+
+
+  // devServer: {
+  //   contentBase: path.join(__dirname, '../server/public'),
+  //   hot: true
+  // }
+
