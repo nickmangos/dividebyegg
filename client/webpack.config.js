d675b86ad8eafea98adde5fa943de42534252078
Nick Mangos <nick.mangos@gmail.com>
2022-06-02 14:23:57 +1200

    Remove attempt to setup fast refresh

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,12 +1,10 @@
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const Dotenv = require('dotenv-webpack')
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 module.exports = {
-  entry: ['./client/index.js', './client/styles/index.scss'],
+  entry: path.join(__dirname, 'index.js'),
   output: {
-    path: path.join(__dirname, '..', 'server', 'public'),
+    path: path.join(__dirname, '../server/public'),
     filename: 'bundle.js'
   },
   mode: 'development',
@@ -15,43 +13,30 @@ module.exports = {
       filename: 'styles.css',
       chunkFilename: '[id].css',
       ignoreOrder: false // Enable to remove warnings about conflicting order
-    }),
-    new Dotenv({
-      path: path.join(__dirname, '..', 'server', '.env')
-    }),
-    new ReactRefreshWebpackPlugin(),
+    })
   ],
   module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          plugins: ['react-refresh/babel'],
-        }
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader
-          },
-          'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+    },
+    {
+      test: /\.(sa|sc|c)ss$/,
+      use: [
+        {
+          loader: MiniCssExtractPlugin.loader
+        },
+        'css-loader',
+        'sass-loader'
+      ]
+    }]
   },
   resolve: {
     extensions: ['.js', '.jsx']
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, '../server/public')
+  }
 }
-
-
-  // devServer: {
-  //   contentBase: path.join(__dirname, '../server/public'),
-  //   hot: true
-  // }
-
