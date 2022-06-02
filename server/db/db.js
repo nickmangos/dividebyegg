d1ad3b15984923599be39a30d9a121e3f7f5e2a5
Nick Mangos <nick.mangos@gmail.com>
2022-06-02 14:28:56 +1200

    Add database funtion to get recipe ingredients

--- server/db/db.js
+++ server/db/db.js
@@ -12,15 +12,49 @@ function addUser (user, db = conn) {
 
 function getRecipe (id, db = conn) {
   return db('recipes')
-    .join('recipe_ingredients', 'recipes.id', 'recipe_ingredients.recipe_id')
-    .join('ingredients', 'recipe_ingredients.ingredient_id', 'ingredients.id')
-    //.select('movies.title', 'reviews.review', 'movies.id')
     .where('recipes.id', id)
     .first()
-}
+} 
+
+function getIngredients (id, db = conn) {
+  return db('recipe_ingredients')
+    .leftJoin('ingredients', 'ingredients.id', 'recipe_ingredients.ingredient_id')
+    .where('recipe_ingredients.recipe_id', id)
+} 
+
+// function getRecipe (id, db = conn) {
+//   return db('recipes')
+//     .join('recipe_ingredients', 'recipes.id', 'recipe_ingredients.recipe_id')
+//     .join('ingredients', 'recipe_ingredients.ingredient_id', 'ingredients.id')
+//     //.select('movies.title', 'reviews.review', 'movies.id')
+//     .where('recipes.id', id)
+//     .first()
+// }
+
+// function getRecipe (id, db = conn) {
+//   return db('recipes')
+//     .where('recipes.id', id)
+//     .first()
+//     .then((x) => {
+//       var temp = {...x}
+//       temp.test = db('ingredients')
+//       return temp
+//     })
+// } 
+
+// knex("mytable")
+// .where({
+//   id: 32423
+// })
+// .del()
+// .then( ->
+//   knex("mytable")
+//   .insert()
+//  ..... 
 
 module.exports = {
   getAllUsers,
   addUser,
-  getRecipe
+  getRecipe,
+  getIngredients
 }
