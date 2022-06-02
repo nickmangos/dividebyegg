3bf0efb35673c89f22c9ba439209787fe4de0a66
Nick Mangos <nick.mangos@gmail.com>
2022-06-02 14:29:54 +1200

    Fix spelling on ingredient in seed

--- server/db/seeds/recipe_ingredients.js
+++ server/db/seeds/recipe_ingredients.js
@@ -1,7 +1,7 @@
 exports.seed = async function(knex) {
   await knex('recipe_ingredients').del()
   await knex('recipe_ingredients').insert([
-    {recipe_id: 3, ingredient_id: null, user_defined: 'Gunness Stout', note: null, quantity: 1, measurement: 'cup', list_order: 1 },
+    {recipe_id: 3, ingredient_id: null, user_defined: 'Gunness stout', note: null, quantity: 1, measurement: 'cup', list_order: 1 },
     {recipe_id: 3, ingredient_id: 1, user_defined: null, note: 'or margarine', quantity: 10, measurement: 'tbsp', list_order: 2 },
     {recipe_id: 3, ingredient_id: 2, user_defined: null, note: 'unsweetened', quantity: 0.75, measurement: 'cup', list_order: 3 },
     {recipe_id: 3, ingredient_id: 3, user_defined: null, note: 'room temperature', quantity: 2, measurement: 'cup', list_order: 4 },
